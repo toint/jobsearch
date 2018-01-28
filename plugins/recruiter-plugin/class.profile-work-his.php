153d585edfe1c0e67bcbc969f3b05fe4bfcfecce
toi.fithou <toi.fithou@gmail.com>
2018-01-29 06:44:47 +0700

    update

---
+++ plugins/recruiter-plugin/class.profile-work-his.php
@@ -0,0 +1,5 @@
+<?php 
+class Profile_Work_His {
+	
+}
+?>
---
+++ plugins/recruiter-plugin/class.user-profile.php
@@ -0,0 +1,15 @@
+<?php 
+class User_Profile {
+	
+	function insert($data) {
+		global $wpdb;
+		
+		$profile = $data['profile'];
+		$driving_licenses = $data['driving_license'];
+		$occupations = $data['occupations'];
+		$levels = $data['levels'];
+		
+	}
+	
+}
+?>
